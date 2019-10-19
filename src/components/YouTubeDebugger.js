e41895f039af036424de1fd94bc186dd54cafbe3
Alan Krajina <alankrajina88@gmail.com>
2019-10-19 09:24:42 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -27,7 +27,7 @@ export default class YouTubeDebugger extends React.Component {
 
   render() {
     return (
-      <button class="bitrate" ></button>
+      <button class="bitrate" onClick={this.updateBitrate}></button>
     );
   }
 }
