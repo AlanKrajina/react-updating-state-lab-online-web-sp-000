5e50398ad84ce3a58ef388362a81d4ce2dc4e908
Alan Krajina <alankrajina88@gmail.com>
2019-10-19 08:59:53 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,19 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+export default class DigitalClicker extends React.Component {
+
+  handleFocus = () => {
+    console.log('Good!');
+  }
+
+  handleBlur = () => {
+    console.log('Hey! Eyes on me!');
+  }
+
+  render() {
+    return (
+      <button  onClick={this.handleClick} ></button>
+    );
+  }
+}
