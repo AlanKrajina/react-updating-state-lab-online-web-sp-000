d12488fe78f10366955b2b38112340c347c6d971
Alan Krajina <alankrajina88@gmail.com>
2019-10-19 09:12:59 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,24 @@
-// Code YouTubeDebugger Component Here
+import React, { Component } from 'react';
+
+export default class YouTubeDebugger extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      timesClicked: 0
+    }
+  }
+
+  handleClick = () => {
+   let newCount = this.state.timesClicked + 1
+   this.setState({
+     timesClicked: newCount
+   })
+ }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+    );
+  }
+}
